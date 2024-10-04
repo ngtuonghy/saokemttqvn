8adfc0d24c428f0b9adf3ff5ba286bd7c573ebc4
ngtuonghy <ngtuonghy@gmail.com>
2024-10-04 21:15:47 +0700

    fix readfile vercel

--- src/actions/read-json.js
+++ src/actions/read-json.js
@@ -1,13 +1,12 @@
 "use server";
-import fs from "fs";
-import path from "path";
+import { promises as fs } from "fs";
 
 const readJsonFile = async () => {
-	const filePath = path.join(process.cwd(), "logs", "changelog.json");
-	console.log(process.cwd());
-
 	try {
-		const data = await fs.promises.readFile(filePath, "utf8");
+		const data = await fs.readFile(
+			process.cwd() + "/logs/changelog.json",
+			"utf8",
+		);
 		return JSON.parse(data);
 	} catch (err) {
 		throw new Error(`Error reading or parsing file: ${err.message}`);
