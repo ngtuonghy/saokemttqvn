d1f67bc255edb345de2912606726c93480c3f157
ngtuonghy <ngtuonghy@gmail.com>
2024-10-05 01:00:51 +0700

    ffix

--- src/actions/read-json.js
+++ src/actions/read-json.js
@@ -1,16 +1,23 @@
 "use server";
 import fs from "fs";
 import path from "path";
+import logs from "@/assets/logs/changelog.json";
 
 const readJsonFile = async () => {
-	const filePath = path.join(process.cwd(), "logs", "changelog.json");
-	console.log(process.cwd());
+	const filePath = path.join(
+		process.cwd(),
+		"public",
+		"assets",
+		"logs",
+		"changelog.json",
+	);
 
 	try {
 		const data = await fs.promises.readFile(filePath, "utf8");
-		return JSON.parse(data);
+		return JSON.parse(data); // Parse the JSON file and return the content
 	} catch (err) {
 		throw new Error(`Error reading or parsing file: ${err.message}`);
 	}
 };
+
 export { readJsonFile };
